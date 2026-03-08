d599cdb8e126c9bde14018b62216c9a93c1fa5d3
BuildTools <unconfigured@null.spigotmc.org>
2026-03-08 19:25:41 +0100

    Přidána lekce 2.2

--- other/vocabulary_editor.py
+++ other/vocabulary_editor.py
@@ -2,29 +2,34 @@
 import json
 import pyperclip #pip install pyperclip
 
-nazev_strany = f"strana_{input('Jaké je číslo strany?: ')}"
+cislo_strany = ""
+while not cislo_strany.isdigit():
+    cislo_strany = input("Jaké je číslo strany?: ").strip()
+nazev_strany = f"strana_{cislo_strany}"
 print(nazev_strany)
 words_list = []
 word_dict = {}
 
 while True:
-    gender = input("Zadejte rod slova: ").strip().lower()
+    gender = None
+    while not (gender == "" or gender in ["der", "die", "das"]):
+        gender = input("Zadejte rod slova: ").strip().lower()
     if gender == "":
         gender = None
-    de = input("Zadejte německé slova: ")
+    de = input("Zadejte německé slova: ").strip()
     de = de.split(";")
-    cz = input("Zadejte české slova: ")
+    cz = input("Zadejte české slova: ").strip()
     cz = cz.split(";")
     description = input("Zadejte popis slova: ").strip()
     if description == "":
         description = None
-    word_dict.update({"cz": cz})
-    word_dict.update({"de": de})
     word_dict.update({"gender": gender})
+    word_dict.update({"de": de})
+    word_dict.update({"cz": cz})
     word_dict.update({"description": description})
     words_list.append(word_dict.copy())
     word_dict.clear()
-    print(de, cz, gender, description)
+    print(gender, de, cz, description)
     if input("Chceš pokračovat? (ne pro ne): ").strip().lower() in ["ne", "n", "no", "nein"]:
         break
 
